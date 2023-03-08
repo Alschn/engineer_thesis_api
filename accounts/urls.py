8bd77fd357ae3bd96553dfa7f7af1279913c43f2
Alschn <67019937+Alschn@users.noreply.github.com>
2023-03-08 12:04:01 +0100

    Add token blacklist endpoint

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,10 @@ from .views import (
     UserRetrieveUpdateAPIView,
     JWTObtainPairView,
     JWTRefreshView,
-    JWTVerifyView
+    JWTVerifyView,
+    UserRegisterAPIView,
+    LogoutView
 )
-from .views.register import UserRegisterAPIView
 
 urlpatterns = [
     path('users/me/', UserRetrieveUpdateAPIView.as_view(), name='users-me'),
@@ -14,4 +15,5 @@ urlpatterns = [
     path('auth/token/refresh/', JWTRefreshView.as_view(), name='token_refresh'),
     path('auth/token/verify/', JWTVerifyView.as_view(), name='token_verify'),
     path('auth/register/', UserRegisterAPIView.as_view(), name='register'),
+    path('auth/logout/', LogoutView.as_view(), name='logout'),
 ]
