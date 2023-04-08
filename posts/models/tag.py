b6083e0da3758d47f8ae082aa90c135298c9e708
Alschn <67019937+Alschn@users.noreply.github.com>
2023-04-08 19:30:17 +0200

    Install `django-colorfield`, add `color` field to Tag

--- posts/models/tag.py
+++ posts/models/tag.py
@@ -1,3 +1,5 @@
+import secrets
+from colorfield.fields import ColorField
 from django.db import models
 
 from core.shared.models import TimestampedModel
@@ -6,6 +8,13 @@ from core.shared.models import TimestampedModel
 class Tag(TimestampedModel):
     tag = models.CharField(max_length=255)
     slug = models.SlugField(db_index=True, unique=True)
+    color = ColorField(default='#000000', blank=True, null=True)
+
+    def save(self, *args, **kwargs) -> None:
+        if not self.color:
+            self.color = f'#{secrets.token_hex(3)}'
+
+        super().save(*args, **kwargs)
 
     def __str__(self) -> str:
         return self.tag
