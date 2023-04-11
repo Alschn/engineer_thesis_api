4d49dfb4ce53ccc94db9d0c7f926a9a0246d84b9
Alschn <67019937+Alschn@users.noreply.github.com>
2023-04-11 12:24:14 +0200

    Update thumbnails upload_to, add static urls to urlpatterns

--- posts/migrations/0004_post_thumbnail.py
+++ posts/migrations/0004_post_thumbnail.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='post',
             name='thumbnail',
-            field=models.ImageField(blank=True, null=True, upload_to='posts/thumbnails'),
+            field=models.ImageField(blank=True, null=True, upload_to='uploads/thumbnails'),
         ),
     ]
